8b5512841aff9da423f88ef6dcf045328a10ff8f
kristinmunn <59896056+kristinmunn@users.noreply.github.com>
2020-03-25 20:11:51 -0700

    Update urls.py

--- pythonClubApp/urls.py
+++ pythonClubApp/urls.py
@@ -4,6 +4,16 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('getmeeting/', views.getmeeting, name='meeting'),
+    path('meeting_details/<int:id>', views.meetingDetails, name='meeting_details'),
+    path('resource/', views.resource, name='resource'),
+    path('event/', views.event, name='event'),
+    path('loginmessage/', views.loginMessage, name='loginmessage'), 
+    path('logoutmessage/', views.logoutMessage, name='logoutmessage'),
+    path('newMeeting/', views.newMeeting, name='newmeeting'),
+    path('newResource/', views.newResource, name='newresource'),
+    path('newMeetingMinutes/', views.newMeetingMinutes, name='newmeetingminutes')
+
 ]
+
 
 
